9cd303e4142ada2c19c695f0a2356ef10c4bb7a2
Pegasso-admon <p3g455o48@gmail.com>
2025-04-22 21:43:50 -0500

    Add files via upload

---
+++ Exercise_1.py
@@ -0,0 +1,10 @@
+#Exercise number 1
+#I need to ask the user for his age
+#If it the age it is above from 18 
+
+edad=int(input("Please input your age:"))
+
+if edad >=18: 
+    print("You are of age.")
+else: 
+    print("You are under age")

7da0524596d463a543e2173c5145ccc7e0252fdc
Pegasso-admon <p3g455o48@gmail.com>
2025-04-23 12:18:19 -0500

    Add files via upload

---
+++ Exercise_7.py
@@ -0,0 +1,5 @@
+#Exercise number 7
+#I need to ask the user to enter two numbers
+#After that, the program will print the bigger one and if the numbers are the same the program will mention it
+
+numbers = int 
